e4c31b6c2454272c1dd67ce8248509c07e18b1d7
davi4261 <david.kludt@rackspace.com>
2018-09-14 13:33:12 -0500

    Add setup file

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='velocity',
+    version='0.1.0',
+    url='https://github.com/oldarmyc/velocity.git',
+    license='BSD',
+    author='Dave Kludt',
+    author_email='oldarmyc@gmail.com',
+    description='Library to interact with swift containers',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'asyncio',
+        'aiohttp',
+        'aiofiles',
+        'requests'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
